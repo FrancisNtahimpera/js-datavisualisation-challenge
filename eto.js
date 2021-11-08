3b2414aa2d715c1524256b095fdf9725d6b66457
francisntahimpera <francisntahimpera@gmail.com>
2021-11-08 14:10:34 +0100

    debut graph2

---
+++ eto.js
@@ -0,0 +1,66 @@
+//
+var referenceNode = document.getElementsByTagName("h4")[2];
+var divGraph2 = document.createElement("canvas");
+divGraph2.setAttribute("id", "graph2");
+
+referenceNode.after(divGraph2);
+
+var tab2 = document.getElementById("table2");
+
+const nb_pays2 = 31;
+
+var labelPays = [];
+var homicide2007 = [];
+var homicide2010 = [];
+var couleur = [];
+
+for (let i = 1; i < nb_pays2; i++) {
+  let tab2_tr = tab2.getElementsByTagName("tr")[i];
+  var tab2_td = tab2_tr.getElementsByTagName("td")[0];
+
+  labelPays.push(tab2_td.innerHTML);
+}
+
+for (let i = 1; i < nb_pays2; i++) {
+  let tab2_tr = tab2.getElementsByTagName("tr")[i];
+  var tab2_td = tab2_tr.getElementsByTagName("td")[1];
+
+  homicide2007.push(tab2_td.innerHTML);
+}
+
+for (let i = 1; i < nb_pays2; i++) {
+  let tab2_tr = tab2.getElementsByTagName("tr")[i];
+  var tab2_td = tab2_tr.getElementsByTagName("td")[2];
+
+  homicide2010.push(tab2_td.innerHTML);
+}
+
+var ctx = document.getElementById("graph2").getContext("2d");
+
+var data = {
+  labels: labelPays,
+  datasets: [
+    {
+      label: "2007",
+      backgroundColor: "rgba(255, 0, 0, 0.4)",
+      data: homicide2007,
+    },
+    {
+      label: "2010",
+      backgroundColor: "rgba(0, 0, 255, 0.4)",
+      data: homicide2010,
+    },
+  ],
+};
+
+var options = {
+  responsive: true,
+};
+
+var config = {
+  type: "bar",
+  data: data,
+  options: options,
+};
+
+var graph2 = new Chart(ctx, config);
